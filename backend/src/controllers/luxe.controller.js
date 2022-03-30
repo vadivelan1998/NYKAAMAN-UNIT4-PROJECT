c38ab9ac34fceb93d94e2c863b53efc5ad273e02
fahadalim <mohammadfahadalim@gmail.com>
2022-03-31 00:45:46 +0530

    commit

--- backend/src/controllers/luxe.controller.js
+++ backend/src/controllers/luxe.controller.js
@@ -1,14 +1,14 @@
-const luxe=require("../models/luxe.model")
+const Luxe=require("../models/luxe.model")
 const express=require("express")
-const authenticate=require("../middlewares/authenticate")
+// const authenticate=require("../middlewares/authenticate")
 
 const router=express.Router()
 
 
-router.get("",authenticate,async(req,res)=>{
+router.get("",async(req,res)=>{
    try {
        const luxe= await Luxe.find({}).lean().exec()
-       return res.status(200).send(personal)
+       return res.status(200).send(luxe)
    } catch (error) {
        return res.status(400).send(error)
    }
--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,8 @@ const { register, login } = require("./controllers/auth.controller");
 
 const personalcareController=require("./controllers/personalcare.controller") 
 const skincareController = require("./controllers/skincare.controller"); 
+const luxeController = require("./controllers/luxe.controller");
+const healthController = require("./controllers/health.controller");  
 
 
 app.post("/register", register);
@@ -18,5 +20,7 @@ app.post("/login", login);
 
 app.use("/personals",personalcareController)
 app.use("/skins", skincareController);
+app.use("/luxe", luxeController);
+app.use("/health", healthController);
 
 module.exports = app;
