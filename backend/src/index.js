24a8e2d0110b79775b3334c17758abd8788af85d
vadivelan1998 <vadivelan1998@gmail.com>
2022-04-01 08:10:48 +0530

    lifestyle and petcare done

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,8 @@ const skincareController = require("./controllers/skincare.controller");
 const luxeController = require("./controllers/luxe.controller");
 const healthController = require("./controllers/health.controller");  
 const addressController = require("./controllers/address.controller"); 
-
+const lifestyleController=require("./controllers/lifestyle.controller")
+const petcareController = require("./controllers/lifestyle.controller");
 
 
 app.post("/register", register);
@@ -26,6 +27,8 @@ app.use("/personals",personalcareController)
 app.use("/skins", skincareController);
 app.use("/luxe", luxeController);
 app.use("/health", healthController);
-app.use("/addresses",addressController)
+app.use("/addresses",addressController);
+app.use("/lifestyles", lifestyleController);
+app.use("/pets", petcareController);
 
 module.exports = app;
