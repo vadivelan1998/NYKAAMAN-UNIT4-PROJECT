9dedfeddaceb7cffcb056db6887aaabde4c0c916
vadivelan1998 <97453410+vadivelan1998@users.noreply.github.com>
2022-04-02 11:21:34 +0530

    Delete backend directory

--- backend/src/configs/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connect = () => {
-  return mongoose.connect(
-    "mongodb+srv://vadivelan1998:vadivelan1234@cluster0.d3xw1.mongodb.net/NYKAAMAN?retryWrites=true&w=majority"
-  );
-};
-
-module.exports = connect;
